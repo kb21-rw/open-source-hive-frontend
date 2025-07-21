c3fec7563ea0f99b3aa93ca81ef5f781a959dd66
kareraolivier <kareraolivier@gmail.com>
2025-07-21 17:15:36 +0200

    feat: add footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="py-[5.9375rem] ">
-      <p className="text-[#007AB9] text-center">@ Open Source Hive - The Gym</p>
+      <p className="text-[#007AB9] text-center text-2xl font-bold">
+        @ Open Source Hive - The Gym
+      </p>
     </div>
   );
 };
