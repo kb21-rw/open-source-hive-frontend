6876c2272f7ec1cf6882218122e8d4dc55b678be
kareraolivier <kareraolivier@gmail.com>
2025-08-18 13:40:45 +0200

    feat: add height of the image

--- components/ImageCard/index.tsx
+++ components/ImageCard/index.tsx
@@ -19,7 +19,7 @@ const ImageCard = ({ hash_tag, header, cards }: ImageCardSection) => {
           </div>
         )}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center md:place-items-start gap-10 lg:gap-20">
         {cards.map((card: ImageCard) => (
           <Card key={card.id} {...card} />
         ))}
